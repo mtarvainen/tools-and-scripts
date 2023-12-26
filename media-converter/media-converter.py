42f4b126ccfd819a56953b4b277e8d6bbf1c79e0
Mikko Tarvainen <mtarvainen@gmail.com>
2023-12-26 14:28:17 +0200

    changed subtitle stream id to default 0. added crf_value option for mp4 video encoding.

--- media-converter/media-converter.py
+++ media-converter/media-converter.py
@@ -45,6 +45,10 @@ def _get_params_by_id(io):
         params = ['-ab', '320k', '-map_metadata', '0', '-id3v2_version', '3']
     if io['output_extension'] in ['sub', 'srt']:
         params = ['-map', f"0:s:{ io['subtitle_stream_id'] }", '-c:s', 'copy']
+    if io['output_extension'] in ['mp4']:
+        params = ['-vcodec', 'libx265', '-x265-params', 'lossless=1', '-preset', 'fast']
+        if io['crf_value'] > 0:
+            params = ['-vcodec', 'libx265', '-crf', f"{ io['crf_value'] }", '-preset', 'fast']
     # if input_extension == 'avi' and output_extension == 'mp4':
     #     pass
     return params
@@ -66,7 +70,8 @@ def main(args):
     io = {
         'input_extension': args.input_extension, 
         'output_extension': args.output_extension, 
-        'subtitle_stream_id': args.subtitle_stream_id
+        'subtitle_stream_id': args.subtitle_stream_id,
+        'crf_value': args.crf_value
     }
     tasks = []
     for dir_item in dirlist:
@@ -84,7 +89,7 @@ def main(args):
             for task in concurrent.futures.as_completed(processes):
                 data = task.result()
                 if not data['processed']:
-                    print(data['stderr'])
+                    print(data)
                 pbar.update(1)
                 #print(f"[ppid={ os.getpid() }, pid={ data['pid'] }]: { data['output_file'] }")
     if args.stty_sane:
@@ -95,7 +100,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input-extension", dest="input_extension", help="Extension of input files eg. flac", action="store", required=True)
     parser.add_argument("-o", "--output-extension", dest="output_extension", help="Extension of output files eg. mp3", action="store", required=True)
-    parser.add_argument("-s", "--subtitle-stream-id", dest="subtitle_stream_id", default=1, help="Subtitle position in media stream.", action="store", required=False)
+    parser.add_argument("-s", "--subtitle-stream-id", dest="subtitle_stream_id", default=0, help="Subtitle position in media stream.", action="store", required=False)
+    parser.add_argument('-c', "--crf-value", type=int, dest="crf_value", default=0, help="H.265 video codec CRF value for MP4 media file. Lower value leads larger file size.", action="store", required=False, choices=range(16,30))
     parser.add_argument("-f", "--force", dest="force", help="Regenerate new version and overwrite old", action="store_true", default=False)
     parser.add_argument("--stty-sane", dest="stty_sane", help="Ensure optimal terminal line settings", action="store_true", default=True)
     parser.add_argument(
